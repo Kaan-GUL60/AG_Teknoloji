75dcb5b18f7184755da183ef4a4cbb3277de0912
GKaan783 <97687364+Kaan-GUL60@users.noreply.github.com>
2023-07-08 16:53:10 +0300

    Update robotik_kol_bluetooth_kontrol.ino

--- robotik_kol_bluetooth_kontrol.ino
+++ robotik_kol_bluetooth_kontrol.ino
@@ -1,4 +1,4 @@
-#include <SoftwareSerial.h>
+
 #include <Servo.h>
 
 Servo myservo1;
@@ -6,111 +6,109 @@ Servo myservo2;
 Servo myservo3;
 Servo myservo4;
 
-#define MotorR1 4
-#define MotorR2 8
-#define MotorRE A2  // analog hız
+const int MotorR1 = 5;  // L298N'in IN3 Girişi
+const int MotorR2 = 9;  // L298N'in IN1 Girişi
 
-#define MotorL1 12
-#define MotorL2 13
+const int MotorL1 = 10;  // L298N'in IN2 Girişi
+const int MotorL2 = 6;   // L298N'in IN4 Girişi
+
+#define MotorRE A2  // analog hız
 #define MotorLE A3  // analog hız
 
-int hiz = 255;
+int hiz = 200;
 
-int bluetoothTx = 10; // bluetooth tx 10 pin
-int bluetoothRx = 11; // bluetooth rx 11 pin
 
-SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
 
 void setup() {
-  myservo1.attach(9);
-  myservo2.attach(8);
-  myservo3.attach(7);
-  myservo4.attach(6);
+  myservo1.attach(8);
+  myservo2.attach(11);
+  myservo3.attach(3);
+  myservo4.attach(4);
   pinMode(MotorR1, OUTPUT);
   pinMode(MotorR2, OUTPUT);
   pinMode(MotorL1, OUTPUT);
   pinMode(MotorL2, OUTPUT);
-
+  pinMode(MotorRE, OUTPUT);
+  pinMode(MotorLE, OUTPUT);
   Serial.begin(9600);
-  bluetooth.begin(9600);
 }
 
 void loop() {
 
-  if (bluetooth.available() > 0) {
-    String receivedString = bluetooth.readString(); // veri okundu
+  if (Serial.available() > 0) {
+    String receivedString = Serial.readString();  // veri okundu
     Serial.println(receivedString);
-
-    String servoName = receivedString.substring(0, 1); // gelen kod okundu
-    String servoValueString = receivedString.substring(2); // boşluktan sonrası okunuyor
-    int servoValue = servoValueString.toInt(); // string integere çevrildi
+    String servoName = receivedString.substring(0, 1);      // gelen kod okundu
+    String servoValueString = receivedString.substring(2);  // boşluktan sonrası okunuyor
+    int servoValue = servoValueString.toInt();              // string integere çevrildi
 
     if (servoName == "a") {
       myservo1.write(servoValue);
-    }
-    else if (servoName == "b") {
+    } else if (servoName == "b") {
       myservo2.write(servoValue);
-    }
-    else if (servoName == "c") {
+    } else if (servoName == "c") {
       myservo3.write(servoValue);
-    }
-    else if (servoName == "d") {
+    } else if (servoName == "d") {
       myservo4.write(servoValue);
-    }
-    else if (servoName == "k") {
-      hiz = servoValue; // Noktalı virgül eklendi
-    }
-    else if (servoName == "1") {
+    } else if (servoName == "k") {
+      hiz = servoValue;  // Noktalı virgül eklendi
+    } else if (servoName == "1") {
       ileri();
-    }
-    else if (servoName == "2") {
+
+    } else if (servoName == "2") {
       dur();
-    }
-    else if (servoName == "3") {
+
+    } else if (servoName == "3") {
       sag();
-    }
-    else if (servoName == "4") {
+
+    } else if (servoName == "4") {
       sol();
-    }
-    else if (servoName == "5") {
+
+    } else if (servoName == "5") {
       geri();
+
     }
+    //receivedString = "";
   }
+
 }
 
 void sag() {
-  digitalWrite(MotorR1, LOW);
-  digitalWrite(MotorR2, HIGH);
-  analogWrite(MotorRE, (hiz - 300));
+  digitalWrite(MotorR1, 1);
+  digitalWrite(MotorR2, 0);
+  analogWrite(MotorRE, (hiz - 150));
 
-  digitalWrite(MotorL1, LOW);
-  digitalWrite(MotorL2, HIGH);
+  digitalWrite(MotorL1, 1);
+  digitalWrite(MotorL2, 0);
   analogWrite(MotorLE, hiz);
-  delay(100); // "bekle" değişkeni yerine sabit bir değer kullanıldı
+  delay(100);  // "bekle" değişkeni yerine sabit bir değer kullanıldı
 }
 
 void sol() {
-  digitalWrite(MotorR1, LOW);
-  digitalWrite(MotorR2, HIGH);
+  digitalWrite(MotorR1, 1);
+  digitalWrite(MotorR2, 0);
   analogWrite(MotorRE, hiz);
 
-  digitalWrite(MotorL1, LOW);
-  digitalWrite(MotorL2, HIGH);
-  analogWrite(MotorLE, (hiz - 300));
-  delay(100); // "bekle" değişkeni yerine sabit bir değer kullanıldı
+  digitalWrite(MotorL1, 1);
+  digitalWrite(MotorL2, 0);
+  analogWrite(MotorLE, (hiz - 150));
+  delay(100);  // "bekle" değişkeni yerine sabit bir değer kullanıldı
 }
 
 void ileri() {
-  digitalWrite(MotorR1, LOW);
-  digitalWrite(MotorR2, HIGH);
+  digitalWrite(MotorR1, 1);
+  digitalWrite(MotorR2, 0);
   analogWrite(MotorRE, hiz);
 
-  digitalWrite(MotorL1, LOW);
-  digitalWrite(MotorL2, HIGH);
+  digitalWrite(MotorL1, 1);
+  digitalWrite(MotorL2, 0);
   analogWrite(MotorLE, hiz);
 }
 
 void dur() {
+  analogWrite(MotorRE, 100);
+  analogWrite(MotorLE, 100);
+  delay(400);
   digitalWrite(MotorR1, LOW);
   digitalWrite(MotorR2, LOW);
 
@@ -119,11 +117,11 @@ void dur() {
 }
 
 void geri() {
-  digitalWrite(MotorR1, LOW);
-  digitalWrite(MotorR2, LOW);
+  digitalWrite(MotorR1, 0);
+  digitalWrite(MotorR2, 1);
   analogWrite(MotorRE, hiz);
 
-  digitalWrite(MotorL1, HIGH);
-  digitalWrite(MotorL2, HIGH);
+  digitalWrite(MotorL1, 0);
+  digitalWrite(MotorL2, 1);
   analogWrite(MotorLE, hiz);
 }
